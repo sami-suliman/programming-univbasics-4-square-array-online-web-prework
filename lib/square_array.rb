d328589ed554fe3d83401155bf6c99338ea6c929
sami suliman <sami.bco@gmail.com>
2019-08-08 22:06:37 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,8 +3,8 @@ def square_array(array)
  new_array = []
  counter = 0 
 
- when counter < array.length  do |integer|
-  new_array << integer ** 2
+ when counter < array.length  do
+  new_array << array(counter) ** 2
  end
  return new_array
 end
